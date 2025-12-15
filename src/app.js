f9c120f5bfde830a765c1f8110530cbb18bd29fa
Nikhilraghuwanshi04 <Nikhilraghuwanshi04@gmail.com>
2025-12-15 17:06:21 +0530

    chore: Install project dependencies.

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect DB
+connectDB();
+
+// routes
+app.use('/api/auth', authRoutes);
+
+// test route
+app.get("/", (req, res) => {
+    res.send("Physio API running");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+);
